7ceee257df162cf66cb6ffe2dfc7b931ea0739fa
Aarushi Maurya <115552907+aarushim7@users.noreply.github.com>
2026-06-18 03:05:32 +0530

    Add files via upload

---
+++ port scanner by Aarushi.py
@@ -0,0 +1,22 @@
+import socket
+
+target = input("Enter IP Address: ")
+
+print(f"\nScanning {target}...\n")
+
+for port in range(1, 101):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(0.5)
+
+        result = s.connect_ex((target, port))
+
+        if result == 0:
+            print(f"Port {port} is OPEN")
+        
+        s.close()
+    except Exception:
+        pass
+
+print("\nScan complete!")
+print("Only open ports are displayed above.")
